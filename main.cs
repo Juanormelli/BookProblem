fbe662cae8fea78da07dae5c4b47c24700696e1f
Juan Medeiros <juan_ormelli@hotmail.com>
2025-06-21 19:33:56 -0300

    fixing format

--- main.cs
+++ main.cs
@@ -4,37 +4,37 @@ using System.Linq;
 
 class Program
 {
-    public static void Main(string[] args)
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("\n" + new string('=', 50));
+
+    Tests.RunAllTests();
+
+  }
+
+  public static decimal NearestSmallDecimal(List<decimal?> bookValues, decimal newBookValue)
+  {
+    if (bookValues == null || bookValues.Count == 0) return decimal.MinValue;
+    if (bookValues.First() > newBookValue) return decimal.MinValue;
+    if (bookValues.Last() < newBookValue) return decimal.MaxValue;
+
+    int left = 0;
+    int right = bookValues.Count - 1;
+    decimal result = decimal.MinValue;
+
+    while (left <= right)
     {
-        Console.WriteLine("\n" + new string('=', 50));
-
-        Tests.RunAllTests();
-        
+      int mid = left + (right - left) / 2;
+      if (bookValues[mid] < newBookValue)
+      {
+        result = (decimal)bookValues[mid];
+        left = mid + 1;
+      }
+      else
+      {
+        right = mid - 1;
+      }
     }
-
-    public static decimal NearestSmallDecimal(List<decimal?> bookValues, decimal newBookValue)
-    {
-        var filtered = bookValues;
-
-        if (bookValues.First() > newBookValue) return decimal.MinValue;
-        if (bookValues.Last() < newBookValue) return decimal.MaxValue;
-
-        int left = 0, right = bookValues.Count - 1;
-        decimal result = decimal.MinValue;
-
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            if (filtered[mid] < newBookValue)
-            {
-                result = (decimal)bookValues[mid];
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
-        }
-        return result;
-    }
+    return result;
+  }
 }
